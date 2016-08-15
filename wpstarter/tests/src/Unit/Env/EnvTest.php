9e7f036b96546a0c9ced717429ee2a0f32f33021
Franz Josef Kaiser <wecodemore@gmail.com>
2016-08-15 13:24:38 +0200

    Tests: Add empty Unit tests file for Env, see #46

---
+++ wpstarter/tests/src/Unit/Env/EnvTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the "" package.
+ *
+ * © 2016 Franz Josef Kaiser
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WCM\WPStarter\Tests\Env;
+
+use WCM\WPStarter\Tests\TestCase;
+use WCM\WPStarter\Env\Env;
+
+class EnvTest
+{
+
+}
